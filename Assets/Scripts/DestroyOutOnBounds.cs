260c3ac0cca05b99cb3bcbc2f66ccb450ddab304
sebascg17 <sebas-clavi@hotmail.com>
2025-05-21 20:13:50 -0500

    Creo el proyecto del juego de la estampida

---
+++ Assets/Scripts/DestroyOutOnBounds.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class DestroyOutOnBounds : MonoBehaviour
+{
+    private float topBound = 40;
+    private float lowerBound = -15;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < lowerBound)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horizontalInput;
+    private float speed = 25.0f;
+    private float xRange = 20.0f;
+    public GameObject proyectilPrefab;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(proyectilPrefab, transform.position, proyectilPrefab.transform.rotation);
+        }
+    }
+}
